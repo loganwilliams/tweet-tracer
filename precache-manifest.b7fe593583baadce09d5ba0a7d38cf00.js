442a72d52a473b53729ee271bfe1697f9bdc5121
Logan Williams <logan.williams@alum.mit.edu>
2020-09-11 10:05:13 -0700

    Updates

---
+++ precache-manifest.b7fe593583baadce09d5ba0a7d38cf00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d49a1908f64c8faa264cd40280154e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "204ff12a76dc2dca8d02",
+    "url": "/static/css/main.726e018d.chunk.css"
+  },
+  {
+    "revision": "162fe0719ec3460b73d8",
+    "url": "/static/js/2.e1d10b1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1d10b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "204ff12a76dc2dca8d02",
+    "url": "/static/js/main.dfc0b387.chunk.js"
+  },
+  {
+    "revision": "b3976d699c037b209c21",
+    "url": "/static/js/runtime-main.78d47ba4.js"
+  }
+]);
